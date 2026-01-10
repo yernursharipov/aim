a927cb25aa7d5e065e78b82449379feb7ceaa67c
Yernur Sharipov <yernursharipov@proton.me>
2026-01-11 01:03:33 +0500

    0.4.0

--- src/aim_version.c
+++ src/aim_version.c
@@ -1,7 +1,7 @@
 #include "aim_version.h"
 
 #define AIM_VERSION_MAJOR 0
-#define AIM_VERSION_MINOR 3
+#define AIM_VERSION_MINOR 4
 #define AIM_VERSION_PATCH 0
 
 #define AIM_VERSION_CONCAT(major, minor, patch) #major "." #minor "." #patch
